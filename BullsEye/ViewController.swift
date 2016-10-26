afac62ad0fcf6b89410533570cac1e685b8336a4
toanhtran <toanh.t.tran@gmail.com>
2016-10-26 20:15:00 +0900

    read the value of the slider

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -31,6 +31,8 @@ class ViewController: UIViewController {
         present(alert, animated: true, completion: nil)
     }
 
-
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        print("The value of th slider is now: \(slider.value)")
+    }
 }
 
