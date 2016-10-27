45c7579618a894e1edba24382a1c9b53eef768cd
toanhtran <toanh.t.tran@gmail.com>
2016-10-27 17:25:25 +0900

    add counter to track each round

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -13,10 +13,12 @@ class ViewController: UIViewController {
     var currentValue = 0
 	var targetValue = 0
 	var score = 0
+	var round = 0
 	
 	@IBOutlet weak var slider: UISlider!
 	@IBOutlet weak var targetLabel: UILabel!
 	@IBOutlet weak var scoreLabel: UILabel!
+	@IBOutlet weak var roundLabel: UILabel!
 	
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -58,6 +60,7 @@ class ViewController: UIViewController {
     }
 	
 	func startNewRound() {
+		round += 1
 		targetValue = 1 + Int(arc4random_uniform(100))
 		currentValue = 50
 		slider.value = Float(currentValue)
@@ -66,6 +69,7 @@ class ViewController: UIViewController {
 	func updateLabels() {
 	targetLabel.text = String(targetValue)
 	scoreLabel.text = String(score)
+	roundLabel.text = String(round)
 	}
 }
 
